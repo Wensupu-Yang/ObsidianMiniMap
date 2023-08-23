edd8b70dcb2636a6ee23e98371cf8d2049f29f8a
Wensupu Yang <wen@Wensupus-MacBook-Pro.local>
2023-08-23 15:53:12 +0100

    Working first commit

--- main.ts
+++ main.ts
@@ -1,134 +1,112 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, ItemView, WorkspaceLeaf } from 'obsidian';
+import * as CodeMirror from 'codemirror';
 
-// Remember to rename these classes and interfaces!
+export const VIEW_TYPE_MINIMAP = "minimap";
 
-interface MyPluginSettings {
-	mySetting: string;
+export class MinimapView extends ItemView {
+    private minimapContainer: HTMLElement;
+    private editor: CodeMirror.Editor;
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+	public closeView() {
+        this.onClose();
+	}
+
+    getViewType(): string {
+        return VIEW_TYPE_MINIMAP;
+    }
+
+    getDisplayText(): string {
+        return "Minimap";
+    }
+
+    getIcon(): string {
+        return "dot-network";
+    }
+
+    async onOpen() {
+        // Initialize the minimap here
+        this.minimapContainer = this.containerEl.createDiv('obsidian-minimap-container');
+
+        // Safely get the current editor instance
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (activeView) {
+            this.editor = activeView.editor as unknown as CodeMirror.Editor;
+
+            // Render the minimap content
+            this.renderMinimap();
+
+            // Sync Minimap with Editor
+            this.syncMinimapWithEditor();
+        } else {
+            // Handle the case where there's no active MarkdownView
+            console.warn("No active MarkdownView found.");
+        }
+    }
+
+    renderMinimap() {
+        const content = this.editor.getValue();
+        const lines = content.split('\n');
+    
+        // Clear previous content
+        this.minimapContainer.empty();
+    
+        // Render each line as a scaled-down version
+        for (let line of lines) {
+            const lineDiv = this.minimapContainer.createDiv('minimap-line');
+            lineDiv.textContent = line;
+        }
+    }
+
+    syncMinimapWithEditor() {
+        // Update the minimap content when the editor content changes
+        this.editor.on("change", () => {
+            this.renderMinimap();
+        });
+
+        // Sync minimap scroll position with editor scroll position
+        this.editor.on("scroll", () => {
+            const scrollInfo = this.editor.getScrollInfo();
+            const scrollPercentage = scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight);
+            this.minimapContainer.scrollTop = scrollPercentage * (this.minimapContainer.scrollHeight - this.minimapContainer.clientHeight);
+        });
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class MinimapPlugin extends Plugin {
+    async onload() {
+        // Register the MinimapView
+        this.registerView(
+            VIEW_TYPE_MINIMAP,
+            (leaf) => new MinimapView(leaf)
+        );
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        // Automatically activate the MinimapView on the right side
+        this.activateMinimapView();
+    }
 
-	async onload() {
-		await this.loadSettings();
+    async activateMinimapView() {
+        // Check if the MinimapView is already active
+        const existingLeaves = this.app.workspace.getLeavesOfType(VIEW_TYPE_MINIMAP);
+        if (existingLeaves.length === 0) {
+            // If not, activate it on the right side
+            await this.app.workspace.getRightLeaf(false).setViewState({
+                type: VIEW_TYPE_MINIMAP,
+                active: true,
+            });
+        }
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+    onunload() {
+        // Clean up any resources or detach views if necessary
+		this.app.workspace.getLeavesOfType(VIEW_TYPE_MINIMAP).forEach((leaf) => {
+			if (leaf.view instanceof MinimapView) {
+				leaf.view.closeView();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_MINIMAP);
+    }
 }
---
+++ temp_old_main.ts
@@ -0,0 +1,335 @@
+// NOT WORKING
+import { App, Editor, MarkdownView, Plugin, ItemView, WorkspaceLeaf } from 'obsidian';
+import * as CodeMirror from 'codemirror';
+
+
+export const VIEW_TYPE_MINIMAP = "minimap";
+
+export class MinimapView extends ItemView {
+    private minimapContainer: HTMLElement;
+    private editor: CodeMirror.Editor;
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+	public closeView() {
+        this.onClose();
+	}
+
+    getViewType(): string {
+        return VIEW_TYPE_MINIMAP;
+    }
+
+    getDisplayText(): string {
+        return "Minimap";
+    }
+
+    getIcon(): string {
+        return "dot-network";
+    }
+
+    async onOpen() {
+        // Initialize the minimap here
+        this.minimapContainer = this.containerEl.createDiv('obsidian-minimap-container');
+
+        // Safely get the current editor instance
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (activeView) {
+            this.editor = activeView.editor.cm;
+
+
+			// Add the debug log here
+			console.log("Fetched editor instance:", this.editor);
+			console.log("Type of activeView.editor:", typeof activeView.editor);
+			console.log("activeView.editor:", activeView.editor);
+
+
+            // Render the minimap content
+            this.renderMinimap();
+
+			// Set the font size for the minimap
+			this.setMinimapFontSize();
+
+            // Sync Minimap with Editor
+            this.syncMinimapWithEditor();
+        } else {
+            // Handle the case where there's no active MarkdownView
+            console.warn("No active MarkdownView found.");
+        }
+    }
+
+    renderMinimap() {
+        const content = this.editor.getValue();
+        const lines = content.split('\n');
+    
+		// Add the debug log here
+		console.log("Rendering minimap with content:", content);
+
+        // Clear previous content
+        this.minimapContainer.empty();
+    
+        // Render each line as a scaled-down version
+        for (let line of lines) {
+            const lineDiv = this.minimapContainer.createDiv('minimap-line');
+            lineDiv.textContent = line;
+        }
+    }
+
+    syncMinimapWithEditor() {
+        // Update the minimap content when the editor content changes
+        this.editor.on("change", () => {
+            this.renderMinimap();
+			this.setMinimapFontSize();
+        });
+
+        // Sync minimap scroll position with editor scroll position
+        this.editor.on("scroll", () => {
+            const scrollInfo = this.editor.getScrollInfo();
+            const scrollPercentage = scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight);
+            this.minimapContainer.scrollTop = scrollPercentage * (this.minimapContainer.scrollHeight - this.minimapContainer.clientHeight);
+        });
+    }
+
+	setMinimapFontSize() {
+		const editorHeight = this.editor.getScrollInfo().height;
+		const minimapHeight = this.minimapContainer.clientHeight;
+		const ratio = minimapHeight / editorHeight;
+	
+		// Assuming the default font size is 16px, adjust based on the ratio.
+		const fontSize = 16 * ratio;
+	
+		this.minimapContainer.style.fontSize = `${fontSize}px`;
+	}
+	
+}
+
+export default class MinimapPlugin extends Plugin {
+    async onload() {
+        // Register the MinimapView
+        this.registerView(
+            VIEW_TYPE_MINIMAP,
+            (leaf) => new MinimapView(leaf)
+        );
+
+        // Automatically activate the MinimapView on the right side
+        this.activateMinimapView();
+    }
+
+    async activateMinimapView() {
+        // Check if the MinimapView is already active
+        const existingLeaves = this.app.workspace.getLeavesOfType(VIEW_TYPE_MINIMAP);
+        if (existingLeaves.length === 0) {
+            // If not, activate it on the right side
+            await this.app.workspace.getRightLeaf(false).setViewState({
+                type: VIEW_TYPE_MINIMAP,
+                active: true,
+            });
+        }
+    }
+
+    onunload() {
+		// Clean up any resources or detach views if necessary
+		this.app.workspace.getLeavesOfType(VIEW_TYPE_MINIMAP).forEach((leaf) => {
+			if (leaf.view instanceof MinimapView) {
+				leaf.view.closeView();
+			}
+		});
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_MINIMAP);
+	}
+}
+
+//-----------------------------------------
+
+import { App, Editor, MarkdownView, Plugin, ItemView, WorkspaceLeaf } from 'obsidian';
+import * as CodeMirror from 'codemirror';
+
+export const VIEW_TYPE_MINIMAP = "minimap";
+
+export class MinimapView extends ItemView {
+    private minimapContainer: HTMLElement;
+    private editor: CodeMirror.Editor;
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+    getViewType(): string {
+        return VIEW_TYPE_MINIMAP;
+    }
+
+    getDisplayText(): string {
+        return "Minimap";
+    }
+
+    getIcon(): string {
+        return "dot-network";
+    }
+
+    async onOpen() {
+        // Initialize the minimap here
+        this.minimapContainer = this.containerEl.createDiv('obsidian-minimap-container');
+
+        // Safely get the current editor instance
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (activeView) {
+            this.editor = activeView.editor as unknown as CodeMirror.Editor;
+
+            // Render the minimap content
+            this.renderMinimap();
+
+            // Sync Minimap with Editor
+            this.syncMinimapWithEditor();
+        } else {
+            // Handle the case where there's no active MarkdownView
+            console.warn("No active MarkdownView found.");
+        }
+    }
+
+    renderMinimap() {
+        const content = this.editor.getValue();
+        const lines = content.split('\n');
+    
+        // Clear previous content
+        this.minimapContainer.empty();
+    
+        // Render each line as a scaled-down version
+        for (let line of lines) {
+            const lineDiv = this.minimapContainer.createDiv('minimap-line');
+            lineDiv.textContent = line;
+        }
+    }
+
+    syncMinimapWithEditor() {
+        // Update the minimap content when the editor content changes
+        this.editor.on("change", () => {
+            this.renderMinimap();
+        });
+
+        // Sync minimap scroll position with editor scroll position
+        this.editor.on("scroll", () => {
+            const scrollInfo = this.editor.getScrollInfo();
+            const scrollPercentage = scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight);
+            this.minimapContainer.scrollTop = scrollPercentage * (this.minimapContainer.scrollHeight - this.minimapContainer.clientHeight);
+        });
+    }
+}
+
+export default class MinimapPlugin extends Plugin {
+    async onload() {
+        // Register the MinimapView
+        this.registerView(
+            VIEW_TYPE_MINIMAP,
+            (leaf) => new MinimapView(leaf)
+        );
+
+        // Automatically activate the MinimapView on the right side
+        this.activateMinimapView();
+    }
+
+    async activateMinimapView() {
+        // Check if the MinimapView is already active
+        const existingLeaves = this.app.workspace.getLeavesOfType(VIEW_TYPE_MINIMAP);
+        if (existingLeaves.length === 0) {
+            // If not, activate it on the right side
+            await this.app.workspace.getRightLeaf(false).setViewState({
+                type: VIEW_TYPE_MINIMAP,
+                active: true,
+            });
+        }
+    }
+
+    onunload() {
+        // Clean up any resources or detach views if necessary
+        this.app.workspace.getLeavesOfType(VIEW_TYPE_MINIMAP).forEach((leaf) => {
+            if (leaf.view instanceof MinimapView) {
+                leaf.view.onClose();
+            }
+        });
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE_MINIMAP);
+    }
+}
+
+
+
+// ------------------------------------------------------------------------------------
+
+import { App, Editor, MarkdownView, Plugin, ItemView, WorkspaceLeaf } from 'obsidian';
+import * as CodeMirror from 'codemirror';
+
+
+// creating a new class for the Minimap
+
+export class MinimapView extends ItemView {
+    private minimapContainer: HTMLElement;
+    private editor: CodeMirror.Editor;
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+    getViewType(): string {
+        return "minimap";
+    }
+
+    getDisplayText(): string {
+        return "Minimap";
+    }
+
+    getIcon(): string {
+        return "dot-network";
+    }
+
+    async onOpen() {
+			// Initialize the minimap here
+			this.minimapContainer = this.containerEl.createDiv('obsidian-minimap-container');
+
+		// Safely get the current editor instance
+			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if (activeView) {
+				this.editor = activeView.editor as unknown as CodeMirror.Editor;
+
+			
+			// Render the minimap content
+			this.renderMinimap();
+
+			// Sync Minimap with Editor
+			this.syncMinimapWithEditor();
+			} else {
+			// Handle the case where there's no active MarkdownView
+			console.warn("No active MarkdownView found.");
+		}
+    }
+
+	onunload() {
+	}
+
+	renderMinimap() {
+		const content = this.editor.getValue();
+		const lines = content.split('\n');
+	
+		// Clear previous content
+		this.minimapContainer.empty();
+	
+		// Render each line as a scaled-down version
+		for (let line of lines) {
+			const lineDiv = this.minimapContainer.createDiv('minimap-line');
+			lineDiv.textContent = line;
+		}
+	}
+
+	syncMinimapWithEditor() {
+        // Update the minimap content when the editor content changes
+        this.editor.on("change", () => {
+            this.renderMinimap();
+        });
+
+        // Sync minimap scroll position with editor scroll position
+        this.editor.on("scroll", () => {
+            const scrollInfo = this.editor.getScrollInfo();
+            const scrollPercentage = scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight);
+            this.minimapContainer.scrollTop = scrollPercentage * (this.minimapContainer.scrollHeight - this.minimapContainer.clientHeight);
+        });
+	
+	}
+}
